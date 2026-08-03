4c8450e4113086281d368d5e0c3d05726d6ce4e3
Om Landge <omlandge0000@gmail.com>
2026-08-03 13:12:15 +0530

    configured backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://choice-enterprises.vercel.app",
+        credentials: true,
+    })
+);
 app.use("/api/auth", authRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/user", userRouter);
