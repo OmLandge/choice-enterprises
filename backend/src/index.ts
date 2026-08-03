040b4ba79340cc73e915fb0e41c6193ee38f3037
Om Landge <omlandge0000@gmail.com>
2026-08-03 13:47:28 +0530

    fixed be cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://choice-enterprises.vercel.app",
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 );
